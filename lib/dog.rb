6d767f9a5b0d236e9fb5153de395826a71c6868f
Yasmin Torres <yasmintorres90@hotmail.com>
2018-11-04 17:24:38 +0000

    automatically backed up by learn

--- lib/dog.rb
+++ lib/dog.rb
@@ -1,15 +1,6 @@
 class Dog 
   def initialize(breed)
-    @name = name
-  end
   
-  def name=(name)
-    @name = name
-  end
-  
-  def name
-    @name
-  end
  
   def breed=(breed)
     @breed = breed
